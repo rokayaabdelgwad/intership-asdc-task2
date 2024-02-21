e85375c829b9ea9a9e7ff1695b9bc6751f2703b9
Rokaya abdelgwad <rokayaabdelgwad246@gmail.com>
2024-02-21 20:52:22 +0200

    change directory of logger error

--- src/modules/logger/logger.module.ts
+++ src/modules/logger/logger.module.ts
@@ -1,9 +1,16 @@
-// logger.module.ts
 import { Module } from '@nestjs/common';
 import { LoggerService } from './logger.service';
 
 @Module({
-  providers: [LoggerService], // Provide LoggerService
-  exports: [LoggerService], // Export LoggerService to be used in other modules
+  providers: [
+    {
+      provide: LoggerService,
+      useFactory: () => {
+        const logDirectory = 'D:\\Projects\\intership asdc\\intership-asdc-task2\\src\\modules\\logger\\error'; 
+        return new LoggerService(logDirectory);
+      },
+    },
+  ],
+  exports: [LoggerService],
 })
 export class LoggerModule {}
